c58dd9c88c71a4d9f5468f5f3dc1b73da4913e95
io <iolande@users.noreply.github.com>
2016-02-25 22:12:57 +0000

    Enable pushstate

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
+    config.options.pushState = true;
     config.map([
       { route: ['', 'books'], name: 'books', moduleId: 'screens/books/books', nav: true, title: 'Books' },
       { route: 'reviews', name: 'reviews', moduleId: 'screens/reviews/reviews', nav: true, title: 'Reviews' },
       { route: 'demos', name: 'demos', moduleId: 'screens/demos/demo-router', nav: true, title: 'Demos' }
     ]);
-
+    router.baseUrl = '/';
     this.router = router;
   }
 }
