306fed6473ae2995feb65ea062a53679c2429b32
io <iolande@users.noreply.github.com>
2016-02-25 22:10:00 +0000

    Add a demo screen for panels

--- src/screens/demos/demo-router.js
+++ src/screens/demos/demo-router.js
@@ -2,7 +2,8 @@ export class DemoRouter {
   configureRouter(config, router) {
     config.title = 'Demos';
     config.map([
-      { route: ['', 'demos'], name: 'demos', moduleId: './demo-index', nav: true, title: 'Demos' }
+      { route: ['', 'demos'], name: 'demos', moduleId: 'screens/demos/demo-index', nav: false, title: 'Demos' },
+      { route: 'panels', name: 'panels', moduleId: 'screens/demos/demo-panels', nav: true, title: 'Panels' }
     ]);
 
     router.baseUrl = "/demos"
