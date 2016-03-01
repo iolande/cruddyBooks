c240e36f6d2d21e48a07e39d71cbaf7df4f12dd1
io <iolande@users.noreply.github.com>
2016-03-01 20:11:43 +0000

    List out the lifecycle events in order (in console)

--- src/screens/demos/demo-index.js
+++ src/screens/demos/demo-index.js
@@ -1,3 +1,37 @@
 export class DemoIndex {
+  // configureRouter() {
+  //   console.log('configureRouter');
+  // }
 
+  canActivate() {
+    console.log('1. router lifecycle - canActivate');
+  }
+
+  activate() {
+    console.log('2. router lifecycle - activate');
+  }
+
+  bind() {
+    console.log('3. viewmodel lifecycle - bind');
+  }
+
+  attached() {
+    console.log('4. viewmodel lifecycle - attached');
+  }
+
+  canDeactivate() {
+    console.log('5. router lifecycle - canDeactivate');
+  }
+
+  deactivate() {
+    console.log('6. router lifecycle - deactivate');
+  }
+
+  detached() {
+    console.log('7. viewmodel lifecycle - detached');
+  }
+
+  unbind() {
+    console.log('8. viewmodel lifecycle - unbind');
+  }
 }
