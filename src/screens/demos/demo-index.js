436268c5f093ffe90295d36b3a610abfcce2316e
io <iolande@users.noreply.github.com>
2016-03-09 18:59:52 +0000

    Update lifecycle info

--- src/screens/demos/demo-index.js
+++ src/screens/demos/demo-index.js
@@ -1,8 +1,4 @@
 export class DemoIndex {
-  // configureRouter(config, router) {
-  //   console.log('configureRouter');
-  // }
-
   // https://www.danyow.net/inversion-of-control-with-aurelia-part-1/
 
   canActivate(params, routeConfig, navigationInstruction) {
@@ -25,46 +21,55 @@ export class DemoIndex {
     console.groupEnd();
   }
 
+  // configureRouter(config, router) {
+  //   console.group('3. configureRouter');
+  //   console.log('router lifecycle');
+  //   console.log('args: config, router');
+  //   console.log(config);
+  //   console.log(router);
+  //   console.groupEnd();
+  // }
+
   created(view) {
-    console.group('3. created');
+    console.group('4. created');
     console.log('viewmodel lifecycle');
     console.log(view);
     console.groupEnd();
   }
 
   bind(bindingcontext) {
-    console.group('4. bind');
+    console.group('5. bind');
     console.log('viewmodel lifecycle');
     console.log(bindingcontext);
     console.groupEnd();
   }
 
   attached() {
-    console.group('5. attached');
+    console.group('6. attached');
     console.log('viewmodel lifecycle');
     console.groupEnd();
   }
 
   canDeactivate() {
-    console.group('6. canDeactivate');
+    console.group('7. canDeactivate');
     console.log('router lifecycle');
     console.groupEnd();
   }
 
   deactivate() {
-    console.group('7. deactivate');
+    console.group('8. deactivate');
     console.log('router lifecycle');
     console.groupEnd();
   }
 
   detached() {
-    console.group('8. detached');
+    console.group('9. detached');
     console.log('viewmodel lifecycle');
     console.groupEnd();
   }
 
   unbind() {
-    console.group('9. unbind');
+    console.group('10. unbind');
     console.log('viewmodel lifecycle');
     console.groupEnd();
   }
