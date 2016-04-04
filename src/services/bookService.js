ec054fc3dc81a019b8bd7e77c92db1d9b3f2ed80
io <iolande@users.noreply.github.com>
2016-04-04 19:21:23 +0100

    Delete commented httpClient and replacer examples

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -40,41 +40,4 @@ export class BookService {
   serviceError(response) {
     console.log(response);
   }
-
-  httpClientConfigInfo() {
-    // withParams - querystring params
-    // withContent
-    // withReviver
-    // withReplacer
-    // withInterceptor
-  }
-
-  withReplacerExample() {
-    // withReplacer([])
-    // Can ensure the request only sends specific data from the full request object
-    // e.g withReplacer(['houseName']) will only send houseName
-
-    // EXAMPLE from their tests
-    // https://github.com/aurelia/http-client/blob/master/test/http-client.spec.js
-    // ---------------------------------------------------------------------------
-    // var content = { firstName: "John", lastName: "Doe" };
-    // var client = new HttpClient()
-    //   .configure(x => {
-    //     x.withBaseUrl(baseUrl);
-    //     x.withHeader('Content-Type', 'application/json');
-    //   });
-
-    // client.createRequest('some/cool/path')
-    //   .asPost()
-    //   .withContent(content)
-    //   .withReplacer(['firstName'])
-    //   .send()
-    //   .then(() => {
-    //     var request = jasmine.Ajax.requests.mostRecent();
-
-    //     expect(request.data()).not.toEqual(content);
-    //     expect(request.data()).toEqual({ firstName: "John" });
-    //     done();
-    //   });
-  }
 }
