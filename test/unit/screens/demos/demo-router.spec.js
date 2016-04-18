6e7add1bb107bbdcd62d3de0bbfac82c3f29d462
io <iolande@users.noreply.github.com>
2016-04-18 19:53:53 +0100

    Add unit tests for demoRouter

--- test/unit/screens/demos/demo-router.spec.js
+++ test/unit/screens/demos/demo-router.spec.js
@@ -1,5 +1,38 @@
 import {DemoRouter} from 'src/screens/demos/demo-router';
 
-describe('DemoRouter', () => {
+class RouterStub {
+  configure(handler) {
+    handler(this);
+  }
 
+  map(routes) {
+    this.routes = routes;
+  }
+}
+
+describe('DemoRouter', () => {
+  var sut;
+  var mockedRouter;
+
+  beforeEach(() => {
+    mockedRouter = new RouterStub();
+    sut = new DemoRouter();
+    sut.configureRouter(mockedRouter, mockedRouter);
+  });
+
+  it('contains a router property', () => {
+    expect(sut.router).toBeDefined();
+  });
+
+  it('configures the router title', () => {
+    expect(sut.router.title).toEqual('Demos');
+  });
+
+  it('should have a demos route', () => {
+    expect(sut.router.routes).toContain({ route: ['', 'demos'], name: 'demos', moduleId: 'screens/demos/demo-index', nav: true, title: 'Demos' });
+  });
+
+  it('should have a panels route', () => {
+    expect(sut.router.routes).toContain({ route: 'panels', name: 'panels', moduleId: 'screens/demos/demo-panels', nav: true, title: 'Panels' });
+  });
 });
