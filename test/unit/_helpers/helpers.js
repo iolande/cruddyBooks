a36b8d7ce94452e175361202a89b2be1e2e92048
io <iolande@users.noreply.github.com>
2016-04-18 22:04:51 +0100

    Add getServiceResponse to helpers

--- test/unit/_helpers/helpers.js
+++ test/unit/_helpers/helpers.js
@@ -3,3 +3,36 @@
 export function isPromise(obj) {
   return !!obj.then && typeof obj.then === 'function';
 }
+
+export function getMockServiceResponse(desiredResponseData) {
+  // Sample books
+  // {"_id":"56d4c09f6bfaafd5923f93e5","author":"Shakuntala Devi","title":"Figuring: The Joy of Numbers","genre":"Mathematics","__v":0,"read":false,"links":{"self":"http://localhost:8000/api/books/56d4c09f6bfaafd5923f93e5"}}
+  // {"_id":"56d4bff66bfaafd5923f93e2","author":"Ian Rankin","title":"The Falls","genre":"Crime","__v":0,"read":false,"links":{"self":"http://localhost:8000/api/books/56d4bff66bfaafd5923f93e2"}}
+  // {"_id":"56e087c6614739abe8129a29","title":"Mathematical Methods in the Physical Sciences","author":"M Boas","genre":"Maths","__v":0,"read":true,"links":{"self":"http://localhost:8000/api/books/56e087c6614739abe8129a29"}}
+
+  return {
+    'requestMessage': {
+      'method': 'GET',
+      'url': 'books',
+      'headers': {
+        'headers': {
+          'Content-Type': 'application/json'
+        }
+      },
+      'baseUrl': 'BASE_URL',
+      'responseType': 'json',
+      'interceptors': [{}]
+    },
+    'statusCode': 200,
+    'response': desiredResponseData,
+    'isSuccess': true,
+    'statusText': 'OK',
+    'mimeType': 'application/json',
+    'headers': {
+      'headers': {
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    },
+    'responseType': 'json'
+  }
+}
