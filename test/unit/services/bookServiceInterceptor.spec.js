a4bae3680b000fd1bb3a2b55844bfa6ff0e551d2
io <iolande@users.noreply.github.com>
2016-04-18 19:59:49 +0100

    Add BookServiceInterceptor unit tests

--- test/unit/services/bookServiceInterceptor.spec.js
+++ test/unit/services/bookServiceInterceptor.spec.js
@@ -2,6 +2,31 @@ import { HttpClient } from 'aurelia-http-client'
 import { BookServiceInterceptor } from 'src/services/bookServiceInterceptor';
 
 describe('BookServiceInterceptor', () => {
-  it('should pass the request straight through');
-  it('should pass the response straight through');
+  let sut;
+
+  beforeEach(() => {
+    sut = new BookServiceInterceptor();
+  });
+
+  afterEach(() => {
+    sut = null;
+  });
+
+  it('should pass the request straight through', () => {
+    let requestMessage = { some: 'message' };
+    let resultMessage = null;
+
+    resultMessage = sut.request(requestMessage);
+
+    expect(resultMessage).toEqual(requestMessage);
+  });
+
+  it('should pass the response straight through', () => {
+    let responseMessage = { some: 'message' };
+    let resultMessage = null;
+
+    resultMessage = sut.response(responseMessage);
+
+    expect(resultMessage).toEqual(responseMessage);
+  });
 });
