03800d878a20d2d0e12c7f79616f396da2d09c54
io <iolande@users.noreply.github.com>
2016-04-20 22:05:40 +0100

    Update book finder tests

--- test/unit/resources/components/forms/book-finder/book-finder.spec.js
+++ test/unit/resources/components/forms/book-finder/book-finder.spec.js
@@ -1,35 +1,38 @@
-import { TemplatingEngine, Container, BindingEngine } from 'aurelia-framework';
-import { MockHttpClient } from '../../../../_helpers/aurelia/mockHttpClient';
-
+import { TemplatingEngine, Container } from 'aurelia-framework';
 import { BookFinder } from '../../../../../../src/resources/components/forms/book-finder/book-finder';
 
-xdescribe('BookFinder', () => {
-  let sandbox;
+fdescribe('BookFinder', () => {
   let container;
-  let bindingEngine;
   let templateEngine;
-
   let bookFinder;
 
   beforeEach(() => {
     container = new Container();
-    container.registerInstance('HttpClient', MockHttpClient);
     templateEngine = container.get(TemplatingEngine);
-    bindingEngine = container.get(BindingEngine);
-
     bookFinder = templateEngine.createViewModelForUnitTest(BookFinder);
   });
 
   afterEach(() => {
     container = null;
-    bindingEngine = null;
     templateEngine = null;
     bookFinder = null;
   });
 
-  it('should be initialized', done => {
+  it('should be initialized', () => {
     expect(bookFinder).not.toBe(null);
     expect(bookFinder).not.toBe(undefined);
-    done();
   });
+
+  describe('searchByGenre', () => {
+    describe('when a search term has been entered', () => {
+      it('should request a list of books for the search term');
+      it('should update the list of filtered books with the response');
+    });
+
+    describe('when no search term has been entered', () => {
+      it('should display a message to the user');
+      it('should not request for a filtered list of books');
+      it('should not change the current list of filtered books');
+    });
+  })
 });
