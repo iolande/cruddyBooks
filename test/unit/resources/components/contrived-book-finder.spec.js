feb4de7af4f706ba7a067eac16c0424665b9150c
io <iolande@users.noreply.github.com>
2016-05-04 22:01:06 +0100

    Add mock classes for the DI - doesn't work
    I want to try out using DI with createViewModelForUnitTest but can't get it to work. It looks correct against the docs but the tests always have the real deps, not the mocks. Perhaps I should wait for the new aurelia-testing package...

--- test/unit/resources/components/contrived-book-finder.spec.js
+++ test/unit/resources/components/contrived-book-finder.spec.js
@@ -1,26 +1,57 @@
-import { TemplatingEngine, Container } from 'aurelia-framework';
+import { Container, TemplatingEngine } from 'aurelia-framework';
 import { ContrivedBookFinder } from '../../../../src/resources/components/contrived-book-finder/contrived-book-finder';
 
-describe('ContrivedBookFinder', () => {
+class Book {
+  title = ''
+  author = ''
+  genre = ''
+  read = false
+
+  update = sinon.stub()
+  clear = sinon.stub()
+}
+
+class DataContext {
+
+}
+
+xdescribe('ContrivedBookFinder', () => {
+  let mockBook;
+  let mockDataContext;
+
   let container;
   let templateEngine;
+
   let sut;
 
   beforeEach(() => {
+    mockBook = new Book();
+    mockDataContext = new DataContext();
+
     container = new Container();
+    container.registerInstance(DataContext, mockDataContext);
+    container.registerInstance(Book, mockBook);
+
     templateEngine = container.get(TemplatingEngine);
     sut = templateEngine.createViewModelForUnitTest(ContrivedBookFinder);
   });
 
   afterEach(() => {
-    container = null;
-    templateEngine = null;
     sut = null;
+    templateEngine = null;
+    container = null;
+    mockBook = null;
+    mockDataContext = null;
   });
 
-  it('should be initialized', () => {
-    expect(sut).not.toBe(null);
-    expect(sut).not.toBe(undefined);
+  describe('constructor', () => {
+    it('should set the expected properties', () => {
+      expect(Object.keys(sut).length).toBe(3);
+
+      expect(sut.booksFilteredByGenre).toBeNull();
+      expect(sut.workingBook).toEqual(new Book());
+      expect(sut.dataContext).toBeNull();
+    });
   });
 
   describe('searchByGenre', () => {
@@ -45,19 +76,6 @@ describe('ContrivedBookFinder', () => {
 
       expect(sut.selectedBook).not.toEqual(selectedBookBeforeTest);
       expect(sut.selectedBook).toEqual(bookForSelection);
-    });
-  });
-
-  describe('searchByGenre', () => {
-    describe('when a search term has been entered', () => {
-      it('should request a list of books for the search term');
-      it('should update the list of filtered books with the response');
-    });
-
-    describe('when no search term has been entered', () => {
-      it('should display a message to the user');
-      it('should not request for a filtered list of books');
-      it('should not change the current list of filtered books');
     });
   });
 });
