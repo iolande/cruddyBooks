7803ffb0e87befaec55a5b179448033aa3a3da7f
io <iolande@users.noreply.github.com>
2016-05-04 21:29:04 +0100

    Add currentBook property to books view

--- src/screens/books/books.js
+++ src/screens/books/books.js
@@ -1,18 +1,26 @@
 import {inject} from 'aurelia-framework';
 import {DataContext} from '../../services/dataContext';
+import {Book as CurrentBook} from '../../models/book';
 
-@inject(DataContext)
+@inject(DataContext, CurrentBook)
 export class Books {
   books = null;
+  currentBook = null;
 
-  constructor(dataContext) {
+  constructor(dataContext, currentBook) {
     this.dataContext = dataContext;
+    this.currentBook = currentBook;
   }
 
   activate() {
     return this.dataContext.getBooks()
       .then(response => this.books = response);
       // .catch(console.log.bind(console)); // What does this do, exactly? #3 in this article https://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html
+  }
+
+  saveCurrentBook() {
+    // console.log(this.currentBook);
+    // return this.dataContext.saveBook(this.currentBook);
   }
 
   addBook(formData) {
