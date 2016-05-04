6dc12359894c6f40a14b09fbef90d807787a2aea
io <iolande@users.noreply.github.com>
2016-05-04 21:27:54 +0100

    Add checkbox custom component

--- src/resources/components/index.js
+++ src/resources/components/index.js
@@ -1,5 +1,6 @@
 export function configure(aurelia) {
   aurelia.globalResources('./forms/book-finder/book-finder');
+  aurelia.globalResources('./forms/checkbox/checkbox');
   aurelia.globalResources('./forms/field-message/field-message');
   aurelia.globalResources('./forms/search-input/search-input');
   aurelia.globalResources('./forms/text-input/text-input');
