5c5892146c44e5a7d92b4cf3f0649d6a6a85f073
io <iolande@users.noreply.github.com>
2016-05-17 23:38:27 +0100

    Use the new custom date component

--- src/screens/demos/demo-converters.js
+++ src/screens/demos/demo-converters.js
@@ -1,3 +1,25 @@
-export class DemoConverters {
+import moment from 'moment';
 
+export class DemoConverters {
+  dateOfBirth = '1980-07-23'
+  dobMessage = {}
+
+  validateDob() {
+    let dob = moment(new Date(this.dateOfBirth));
+
+    if (!dob.isValid()) {
+      this.dobMessage = { message: 'You must enter a dateOfBirth', level: 'error' };
+      return false;
+    }
+
+    if (dob.diff(new Date(), 'years') < 18) {
+      this.dobMessage = { message: 'User must be over 18', level: 'error' };
+      return false;
+    }
+
+    if (dob.diff(new Date(), 'years') > 100) {
+      this.dobMessage = { message: 'User must be under 100', level: 'error' };
+      return false;
+    }
+  }
 }
